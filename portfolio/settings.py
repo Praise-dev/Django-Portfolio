2a6d7edccbf1261d40379b6606f3b77f3e9942c0
Praise <opraiz6@gmail.com>
2019-07-03 18:45:22 -0500

    after extends tag

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["jobs/templates/jobs"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
